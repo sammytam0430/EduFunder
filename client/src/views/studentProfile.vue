a8c0399a87c35ee8a7391f139be8b0a7671ef88d
Jun <jun.jeong95@gmail.com>
2019-11-12 23:32:47 -0800

    student profile page added

---
+++ client/src/views/studentProfile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wrapper">
+    <div>
+        <div>
+            hello world
+        </div>
+    </div>  
+  </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
